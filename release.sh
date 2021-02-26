5d9308fb59abea30b79ac2e49cfc7ea13a2fe251
Ivar Refsdal <ivar.refsdal@nsd.no>
2021-02-26 15:52:55 +0100

    Update release script

--- release.sh
+++ release.sh
@@ -5,21 +5,27 @@ set -ex
 clojure -Spom
 clojure -M:jar
 
+cd backend && clojure -Spom && clojure -M:jar && cd ..
+
 COMMIT_COUNT="$(git rev-list --count HEAD)"
 let "NEXT_PATCH=COMMIT_COUNT+1"
+NEW_VERSION="$(date '+%Y.%m.%d')-alpha$NEXT_PATCH"
 
-sed -i 's|^  <version>\(.*\..*\.\).*</version>$|  <version>\1'$NEXT_PATCH'</version>|' pom.xml
+echo "Releasing $NEW_VERSION ..."
 
-RAW_VERSION="$(grep "^  <version>" pom.xml | sed 's|^.*<version>\(.*\)</version>.*$|\1|')"
-
-NEW_VERSION="v$RAW_VERSION"
-echo "Releasing >$NEW_VERSION< ..."
+sed -i 's|^  <version>.*</version>$|  <version>'$NEW_VERSION'</version>|' pom.xml
+sed -i 's|^  <version>.*</version>$|  <version>'$NEW_VERSION'</version>|' backend/pom.xml
+sed -i 's|^    <tag>.*$|    <tag>v'$NEW_VERSION'</tag>|' pom.xml
+sed -i 's|^    <tag>.*$|    <tag>v'$NEW_VERSION'</tag>|' backend/pom.xml
 
 git add pom.xml
-git commit -m "Release $NEW_VERSION"
-git tag -a $NEW_VERSION -m "Release $NEW_VERSION"
+git add backend/pom.xml
+git commit -m "Release v$NEW_VERSION"
+git tag -a "v$NEW_VERSION" -m "Release v$NEW_VERSION"
 git push --follow-tags
 
 clojure -M:deploy
 
-echo "Released $RAW_VERSION!"
+cd backend && clojure -M:deploy && cd ..
+
+echo "Released $NEW_VERSION!"
